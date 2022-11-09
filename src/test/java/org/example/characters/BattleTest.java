596527ce20d018c7bedfed17c44a734a5e092f8c
Martin0071 <marto007.1998@gmail.com>
2022-11-09 21:19:50 +0200

    Added junit5 tests

--- src/test/java/org/example/characters/BattleTest.java
+++ src/test/java/org/example/characters/BattleTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BattleTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     @DisplayName("Fight")
     void firstFight() {
         Warrior carl = new Warrior();
